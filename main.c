1dff4318f616e592c2de9b21d57df84b4d700510
Bobbbay <abatterysingle@gmail.com>
2020-11-01 23:16:53 -0500

    Updated code

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <memory.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char** argv) {
+    char    buff[BUFSIZ];
+    FILE    *in, *out;
+    size_t  n;
+
+    char dest[40] = "/semc/coreutils/bak/";
+    strcat(dest, argv[1]);
+    strcat(dest, ".semc.bak");
+    printf(dest);
+
+
+    in = fopen( argv[1], "rb" );
+    out= fopen( dest, "wb" );
+    while ( (n=fread(40,1,BUFSIZ,in)) != 0 ) {
+        fwrite( buff, 1, n, out );
+    }
+
     return 0;
 }
